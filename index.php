f29392f9f058ac2ad9b9394a1b669be7f374c55f
Jhontabo <jhonse.tajumbina@gmail.com>
2024-04-30 18:14:53 -0500

    Se completo con exito el footer de la pagina principal

--- index.php
+++ index.php
@@ -7,162 +7,124 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
+    <meta name="description" content="Pagina de pedidos para el restaurante luna verde">
+    <meta name="author" content="Jhontabo">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <title>Restaurante Luna verde</title>
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/business-casual.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
-	
-	<?php
-		$Username = null;
-		if(!empty($_SESSION["Username"]))
-		{
-			$Username = $_SESSION["Username"];
-		}
-	?>
+
+    <?php
+    $Username = null;
+    if (!empty($_SESSION["Username"])) {
+        $Username = $_SESSION["Username"];
+    }
+    ?>
 </head>
 
 <body>
-    <div class="brand">Entregas Luna verde</div>
-    <div class="address-bar">Directo y a la Puerta de tu Casa</div>
 
-    <nav class="navbar navbar-default" role="navigation">
+    <header>
+        <h2>Luna verde</h2>
+    </header>
+
+    <nav class="navbar">
         <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.html">Entregas ConfiguroWeb</a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a href="index.php">Inicio</a></li>
-                    <li><a href="about.php">Nosotros</a></li>
-					<li><a href="#" onclick="ManagementOnclick();">Administrador</a><li>
-
-					<?php if($Username == null){echo '<li><a href="register.php?ActionType=Register">Registrarse</a></li>';} ?>
-                    <?php if($Username != null){echo '<li><a href="ManageAccount.php?Role=User">Cuenta</a></li>';} ?> 
-					<?php if($Username == null){echo '<li><a href="Login.php?Role=User">Ingresar</a></li>';} else {echo '<li><a href="Logout.php">Logout</a></li>';} ?>
-                </ul>
-            </div>
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="about.php">Nosotros</a></li>
+                <li><a href="#" onclick="ManagementOnclick();">Administrador</a>
+            </ul>
+        </div>
+        </div>
         </div>
     </nav>
 
-    <div class="container">
-        <div class="row">
-            <div class="box">
-                <div class="col-lg-12 text-center">
-                    <div id="carousel-example-generic" class="carousel slide">
-                        <ol class="carousel-indicators hidden-xs">
-                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-							<li data-target="#carousel-example-generic" data-slide-to="3"></li>
-                        </ol>
+    <?php
+    $conn = mysqli_connect("localhost", "root", "", "smss_db");
+    $sql = "SELECT * FROM `tbl_products` Limit 50";
+    $Resulta = mysqli_query($conn, $sql);
+    ?>
 
-                        <div class="carousel-inner">
-                            <div class="item active">
-                                <img class="img-responsive img-full" src="img/slide-1.jpg" alt="">
-                            </div>
-                            <div class="item">
-                                <img class="img-responsive img-full" src="img/slide-2.jpg" alt="">
-                            </div>
-                            <div class="item">
-                                <img class="img-responsive img-full" src="img/slide-3.jpg" alt="">
-                            </div>
-							<div class="item">
-                                <img class="img-responsive img-full" src="img/slide-4.jpg" alt="">
-                            </div>
+
+    <?php while ($Rows = mysqli_fetch_array($Resulta)) {
+        echo '	
+		<div class="col-sm-4 col-lg-4 col-md-4">
+             <div class="thumbnail">
+				<h4 style="text-align: center;">' . $Rows[2] . '</h4>
+                <img style="border: 2px solid gray; border-radius: 10px; height: 229px; width: 298px;" src="data:image;base64,' . $Rows[8] . '" alt="">
+                <div class="caption">
+					<p><strong>Nombre del Producto:</strong> ' . $Rows[1] . '</p>
+					<p><strong>Dimensiones:</strong> ' . $Rows[3] . '</p>
+					<p><strong>Colores Disponibles:</strong> ' . $Rows[4] . '</p>
+					<p><strong>$ ' . $Rows[5] . '.00</strong></p>
+                </div>
+				<center><a onclick="addToCartOnclick(' . $Rows[0] . ');" href="#"  style="margin-bottom: 5px;" class="btn btn-primary">Agregar al Carrito</a></center>
+            </div>
+        </div>
+		';
+    } ?>
+
+    </div>
+    <footer>
+        <div class="container">
+            <div class="footer-grid">
+                <div class="luna-verde">
+                    <h2>Luna <span class="verde">verde</span></h2>
+                    <p>
+                        Sumérgete en la auténtica tradición culinaria con nuestro delicioso
+                        sabor casero, cuidadosamente elaborado para brindarte una experiencia
+                        gastronómica reconfortante y llena de nostalgia. En cada bocado, encontrarás la esencia
+                        de los sabores familiares, preparados con pasión y dedicación, como si estuvieras
+                        disfrutando de una comida en casa.</p>
+                    <p class="copyright">2009 - 2024</p>
+                    <div class="redes-sociales">
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#" class="whatsapp"><i class="fab fa-whatsapp"></i></a>
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                    </div>
+                </div>
+                <div class="horarios">
+                    <h4>Horarios</h4>
+                    <ul>
+                        <ul>
+                            <li>Lunes</li>
+                            <li>7:00 AM - 6:00 PM</li>
+                            <li>Martes</li>
+                            <li>7:00 AM - 6:00 PM</li>
+                            <li>Miércoles</li>
+                            <li>7:00 AM - 6:00 PM</li>
+                            <li>Jueves</li>
+                            <li>7:00 AM - 6:00 PM</li>
+                            <li>Viernes</li>
+                            <li>7:00 AM - 6:00 PM</li>
+                            <li>Sábado</li>
+                            <li>7:00 AM - 3:00 PM</li>
+                            <li>Domingo</li>
+                            <li>CERRADO</li>
+                        </ul>
+                    </ul>
+                </div>
+                <div class="direccion-telefono">
+                    <div class="direccion">
+                        <h4>Direccion</h4>
+                        <p>Dirección: Cra 34 #14-12, Pasto, Nariño</p>
+                        <div class="mapa">
+                            <!-- Aquí iría el código para insertar el mapa -->
                         </div>
-                        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                            <span class="icon-prev"></span>
-                        </a>
-                        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                            <span class="icon-next"></span>
-                        </a>
+                    </div>
+                    <div class="telefono">
+                        <h4>Telefono</h4>
+                        <p>+57 300 208 1134</p>
                     </div>
                 </div>
             </div>
         </div>
-		
-		<?php 
-			$conn = mysqli_connect("localhost","root","","smss_db");
-			$sql = "SELECT * FROM `tbl_products` Limit 100";
-			$Resulta = mysqli_query($conn,$sql);
-		?>
-		
-		
-		<?php while($Rows = mysqli_fetch_array($Resulta)){
-		echo '	
-		<div class="col-sm-4 col-lg-4 col-md-4">
-             <div class="thumbnail">
-				<h4 style="text-align: center;">'.$Rows[2].'</h4>
-                <img style="border: 2px solid gray; border-radius: 10px; height: 229px; width: 298px;" src="data:image;base64,'.$Rows[8].'" alt="">
-                <div class="caption">
-					<p><strong>Nombre del Producto:</strong> '.$Rows[1].'</p>
-					<p><strong>Dimensiones:</strong> '.$Rows[3].'</p>
-					<p><strong>Colores Disponibles:</strong> '.$Rows[4].'</p>
-					<p><strong>$ '.$Rows[5].'.00</strong></p>
-                </div>
-				<center><a onclick="addToCartOnclick('.$Rows[0].');" href="#"  style="margin-bottom: 5px;" class="btn btn-primary">Agregar al Carrito</a></center>
-            </div>
-        </div>
-		';
-		}?>
-		
-	</div>
-
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <p>
-					<?php echo '<strong>'.$Username.'</strong>'; ?>
-					<br>
-					<strong> 
-					<?php if($Username == null){echo '<a href="Login.php?Role=User">Ingresar</a>';} else {echo '<a href="Logout.php">Logout</a>';} ?> | 
-					<a href="#">Volver al inicio</a>
-					</strong><br>
-					Copyright &copy; ConfiguroWeb
-					</p>
-					
-                </div>
-            </div>
+        <div class="creditos">
+            <p>2022 - Diseñado por Juan Jose Pachajoa - Daniel Luna - Sara Valentina Macuace - Jhon Tajumbina - Cristian
+                Ortega</p>
         </div>
     </footer>
-
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script>
-		$('.carousel').carousel({
-			interval: 5000 //changes the speed
-		})
-		
-		$('#reg').click(function(){
-			window.open('register.html',_self);
-		});
-		
-		function ManagementOnclick(){
-			if(confirm("Solo los administradores tienen permitido acceder a esta página. Inicie sesión como administrador.") == true)
-			{
-				window.open("Login.php?Role=Admin","_self",null,true);
-			}
-		}
-		
-		function addToCartOnclick(ProductID)
-		{	
-			if(confirm("Estas seguro que deseas agregar este producto al carrito") == true){
-			window.open("Order.php?ProductID="+ProductID,"_self",null,true);
-			}
-		}
-    </script>
 </body>
 
 </html>
