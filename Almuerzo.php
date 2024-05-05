25a2c430f1425bc2acfce6aaa857018358e63743
Jhontabo <jhonse.tajumbina@gmail.com>
2024-05-04 22:29:29 -0500

    frontend terminado , en cuanto a los prototipos

--- Almuerzo.php
+++ Almuerzo.php
@@ -19,53 +19,45 @@ $Hola = "Hola";
 
 <body>
 
-  <div class="contenedor-principal">
     <header class="cabecera">
-      <h1 class="titulo"><span class="verde">Almu</span><span class="naranja">erzos</span></h1>
+      <h1 class="titulo"><span class="verde">Desa</span><span class="naranja">yunos</span></h1>
       <img src="./img/logoLunaVerde.png" alt="Logo" class="logo">
     </header>
 
-    <main class="contenido">
+    <div class="contenido">
+      <div class="seccion-principal">
+        <img src="./img/almuerzo1.jpeg" alt="Desayuno principal" class="circulo-central">
+      </div>
 
-      <section class="seccion-principal">
-        <img src="./img/Desayuno1.jpg" alt="Desayuno principal" class="circulo-central">
-      </section>
+      <div class="opciones-desayuno">
+        <div class="desayuno">
+          <img src="./img/almuerzo1.jpeg" alt="Desayuno completo" onclick="cambiarImagen('./img/almuerzo1.jpeg')">
+          <p class="calorias">850 calorias</p>
+          <button onclick="redirigirAPedir()" class="precio">$4.400</button>
+        </div>
 
-      <section class="opciones-desayuno">
-        <article class="desayuno">
-          <img src="./img/Desayuno2.jpg" alt="Desayuno completo" onclick="cambiarImagen('./img/Desayuno2.jpg')">
-            <p class="calorias">850 calorias</p>
-
-          <div class="precio-contenedor">
-            <button onclick="redirigirAPedir()" class="precio">$4.000</button>
-          </div>
-
-        </article>
-
-        <article class="desayuno">
-          <img src="./img/Desayuno3.jpg" alt="Desayuno intermedio" onclick="cambiarImagen('./img/Desayuno3.jpg')">
+        <div class="desayuno">
+          <img src="./img/almuerzo2.jpeg" alt="Desayuno intermedio" onclick="cambiarImagen('./img/almuerzo2.jpeg')">
           <p class="calorias">550 calorias</p>
-          <div class="precio-contenedor">
-          <button onclick="redirigirAPedir()" class="precio">$4.000</button>
-          </div>
-        </article>
+          <button onclick="redirigirAPedir()" class="precio">$8.500</button>
+        </div>
 
-        <article class="desayuno">
-          <img src="./img/Desayuno1.jpg" alt="Desayuno basico" onclick="cambiarImagen('./img/Desayuno1.jpg')">
+        <div class="desayuno">
+          <img src="./img/almuerzo3.jpeg" alt="Desayuno basico" onclick="cambiarImagen('./img/almuerzo3.jpeg')">
           <p class="calorias">181 calorias</p>
-          <div class="precio-contenedor">
-            <button onclick="redirigirAPedir()" class="precio">$4.000</button>
-          </div>
-        </article>
-      </section>
-    </main>
+          <button onclick="redirigirAPedir()" class="precio">$10.000</button>
+        </div>
+      </div>
+    </div>
+
     <footer class="pie-pagina">
       <button class="boton-salir" onclick="redirigirInicio()">Regresar</button>
     </footer>
-  </div>
+
 </body>
 
 <script src="./js/script.js"></script>
 <script src="./js/regresar.js"></script>
+<script src="./js/pedido.js"></script>
 
 </html>
