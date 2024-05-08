4ca86fa8ac6235ddc4e5f6b45ec1ce33fa402d8d
Jhontabo <jhonse.tajumbina@gmail.com>
2024-05-08 18:08:49 -0500

    redireciones listas

--- RegistrarPedido.php
+++ RegistrarPedido.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Registrar Pedido</title>
-<link href="https://fonts.googleapis.com/css?family=Rancho" rel="stylesheet">
-<link rel="stylesheet" href="./css/RegistrarPedido.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Registrar Pedido</title>
+  <link href="https://fonts.googleapis.com/css?family=Rancho" rel="stylesheet">
+  <link rel="stylesheet" href="./css/RegistrarPedido.css">
 </head>
 <body>
   <div class="registro-container">
@@ -14,18 +14,23 @@
       <input type="text" placeholder="Nombre" required>
       <input type="text" placeholder="Número de documento" required>
       <input type="text" placeholder="Dirección" required>
-      <input type="tel" placeholder="Teléfono" required>
+      <input type="tel"  placeholder="Teléfono" required>
       <input type="number" placeholder="Cantidad" required>
-      <select name="forma-pago">
-        <option value="visa">Tiquetera</option>
-        <option value="mastercard">Efectivo</option>
-        <option value="mastercard">Nequi</option>
-      </select>
+
+      <div class="forma-pago">
+        <h2>Forma de pago</h2>
+        <select name="forma-pago">
+          <option value="tiquetera">Tiquetera</option>
+          <option value="efectivo">Efectivo</option>
+          <option value="nequi">Nequi</option>
+        </select>
+      </div>
       <div class="btn-group">
-        <button type="submit" class="btn-registrar">Registrar</button>
-        <button type="button" class="btn-salir">Salir</button>
+        <button type="submit" class="btn-registrar" >Registrar</button>
+        <button class="btn-salir" onclick="redirigirInicio()">Salir</button>
       </div>
     </form>
   </div>
 </body>
+<script src="./js/redireciones.js"></script>
 </html>
